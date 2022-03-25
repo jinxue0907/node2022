0e08921b84225884126ced25dea5ec2e3ef7ba56
jinxue <w2024@e-mirim.hs.kr>
2022-03-25 14:47:04 +0900

    web2_5 정적 서버 페이지

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const http = require('http')
+const fs = require("fs")
 
-const app = http.createServer(function (IncomingMessage, ServerResponse){
+const app = http.createServer(function (req, res){
+   let url = req.url;
+
+   if(url === '/')
+      url = '/index.html';
+   if(url === '/favicon.ico')
+      return res.writeHead(404);
+
    res.writeHead(200);
-   console.log(__dirname + req.url);
-   res.end("Hello World");
+   res.end(fs.readFileSync(__dirname + url));
 });
 
-app.listen(3000);
+app.listen(3333);
