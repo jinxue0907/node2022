cbab4e3313348b34cb5dc8a1d209c012cf9d031e
jinxue <w2024@e-mirim.hs.kr>
2022-04-15 14:57:13 +0900

    web2-36 글수정-파일명(게시글제목) 변경, 변경내용저장

--- index.js
+++ index.js
@@ -97,6 +97,47 @@ const app = http.createServer(function (request, response) {
       // response.writeHead(200);
       // response.end('success');
    }
+   else if(pathname === '/update') {
+      // data : 실제 파일리스트 문자열들의 배열 (파일이름은 게시글의 제목)
+      fs.readdir('data', function (err, data){
+         //  description : 파일안의 내용물(게시글의 내용)
+         fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description){
+            const title = queryData.id;
+            const list = templateList(data);
+            const template = templateHTML(title, list, `
+                <form action="/update_process" method="post">
+                <input type="hidden" name="id" value="${title}"
+                    <p><input type="text" name="title" placeholder="title" value="${title}"> </p>
+                    <p><textarea name="description" placeholder="description">${description}</textarea></p>                
+                    <p><input type="submit"></p>
+                </form>`, `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`)  // 글생성 중에는 create, update가 안나오게
+            response.writeHead(200);
+            response.end(template)
+         });
+      });
+   }
+   else if(pathname === '/update_process'){
+      // 넘겨받은 데이터를 문자열 형태로 body에 축적
+      let body = '';
+      request.on('data', function (data){
+         body += body + data;
+      });
+      request.on('end', function (){
+         const post = qs.parse(body);
+         const id = post.id;        // 바꾸기 전의 파일이름(게시글 제목)
+         const title = post.title;        //바꾼 이후의 파일이름(게시글 제목)
+         const description = post.description;
+         fs.rename(`data/${id}`, `data/${title}`, function(err){
+            fs.writeFile(`data/${title}`, description, 'utf-8', function (err) {
+               response.writeHead(302, {Location: `/?id=${title}`});
+               response.end();
+            })
+         })
+
+
+
+         });
+   }
    else {
       response.writeHead(404)
       response.end('Not found')
